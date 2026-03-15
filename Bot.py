73c6b93c2b449f785fe326047e0bb61a78944a7e
Catzey <sahalsanah12@gmail.com>
2026-03-15 12:27:15 +0530

    Add message handler to reply to all messages

--- Bot.py
+++ Bot.py
@@ -8,7 +8,9 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
 await update.message.reply_text("Bot working!")
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(MessageHandler(filters.ALL, reply))
 
 print("Bot started")
+
 app.run_polling()
